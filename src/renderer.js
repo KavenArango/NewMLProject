29a8a6ce2e1d48f4129909c72dd71ffd6edb5348
Kaven arango <43826797+KavenArango@users.noreply.github.com>
2020-12-01 01:09:43 -0500

    packager

--- src/renderer.js
+++ src/renderer.js
@@ -3,5 +3,4 @@ const openBtn = document.getElementById('files')
 
 openBtn.addEventListener('click', function (event) {
     shell.openPath(path.join(__dirname, '../JNotebook'))
-    console.log("clicked me")
 })
